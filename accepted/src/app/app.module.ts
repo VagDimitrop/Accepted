9be9ac2f2a0442e7f3dc6ddebaf4cdb8f8015e2c
Vaggelis Dimitropoulos <vag.dimitrop@gmail.com>
2024-02-21 20:27:21 +0200

    Adds bootstrap to project | Adds styling to list component

--- accepted/src/app/app.module.ts
+++ accepted/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
   declarations: [
     AppComponent,
     DropdownListComponent,
+    ListComponent,
     HomeComponent,
     DetailsComponent
   ],
