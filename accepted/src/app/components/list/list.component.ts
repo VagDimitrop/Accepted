4278d6e9887afdfd11355f4f493dfe78bbf5f664
Vaggelis Dimitropoulos <vag.dimitrop@gmail.com>
2024-02-23 17:12:11 +0200

    Adds functionality to pass data of clicked team from homePage to detailsPage

--- accepted/src/app/components/list/list.component.ts
+++ accepted/src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Team} from "../../interfaces/teams.interface";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list',
@@ -11,4 +12,9 @@ export class ListComponent {
   @Input() teams!: Team[];
   @Input() dataHasLoaded!: boolean;
 
+  constructor(private router: Router) {}
+
+  handleClick(team: Team) {
+      this.router.navigate(['/details'],{state: {team}});
+  }
 }
